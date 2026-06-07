01f52b17549f42e345ce6859221786d49ff6856d
elvitigalalis <elvitigalalis@gmail.com>
2026-06-07 01:20:17 -0400

    Add SwiftUI app entry point

---
+++ apps/macos-client/Sources/Idea_Evolution_EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Idea_Evolution_EngineApp.swift
+//  Idea Evolution Engine
+//
+//  Created by Lisul Elvitigala on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Idea_Evolution_EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
